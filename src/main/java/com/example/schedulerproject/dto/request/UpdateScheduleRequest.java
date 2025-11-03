3845b1afd82a5af0cb82f159ddd326bdd3423caf
sseung0510 <choiseunghi0510@gmail.com>
2025-11-03 20:26:40 +0900

    lv3 - 일정 수정 기능

---
+++ src/main/java/com/example/schedulerproject/dto/request/UpdateScheduleRequest.java
@@ -0,0 +1,11 @@
+package com.example.schedulerproject.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateScheduleRequest {
+    private String title;
+    private String userName;
+    private String userPwd;
+
+}
