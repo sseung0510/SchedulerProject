b55069ab769fbd496d0f35252c896085380eec33
sseung0510 <choiseunghi0510@gmail.com>
2025-11-03 19:02:42 +0900

    일정 관리 앱 프로젝트 시작

---
+++ src/main/java/com/example/schedulerproject/SchedulerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulerproject/SchedulerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
